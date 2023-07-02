7972dd030d9185390cbe3858fe07b49840817ffc
Dhanush Saji <dhanushsaji1@gmail.com>
2023-07-03 00:37:14 +0530

    03-07

--- src/app/movies/page.jsx
+++ src/app/movies/page.jsx
@@ -1,31 +1,9 @@
-import React from 'react'
+"use client";
+
+import React,{useState,useEffect} from 'react'
 import axios from 'axios';
 import styles from '@/app/styles/common.module.css'
-const page = () => {
-  let movieData;
-  const getDataFn = async() =>{
-    try {
-      const response = await axios.get(`https://netflix54.p.rapidapi.com/search/`,{params: {
-        query: 'stranger',
-        offset: '0',
-        limit_titles: '50',
-        limit_suggestions: '20',
-        lang: 'en'
-      },
-      headers: {
-        'X-RapidAPI-Key': process.env.NETFLIXAPIKEY,
-        'X-RapidAPI-Host': 'netflix54.p.rapidapi.com'
-      }
-    })
-    movieData = await response.data
-    const main_data = movieData.titles;
-    console.log(main_data?.jawSummary)
-
-    } catch (error) {
-      
-    }
-  }
-  getDataFn()
+const page = async() => {
   return (
     <>
     <section className={`${styles.movieSection}`}>
@@ -34,7 +12,7 @@ const page = () => {
       
     <h1>Movie Pages</h1>
     {
-      movieData?.titles?.map((item)=>(
+      data?.map((item)=>(
         <>
         {/* <MovieCard key={item.id} item={item} /> */}
         <h1>hi</h1>
